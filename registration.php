99913671c08108f7734a3634795b673169cfac08
Denis Aleksandrov <denis.v.aleksandrov@gmail.com>
2023-10-26 12:32:34 +0200

    Created required files for registering the module with Magento. 	modified:   README.md 	new file:   composer.json 	new file:   etc/module.xml 	new file:   registration.php

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Denal05_MacademyCustomizeM2Checkout',
+    __DIR__,
+);
