92f00deb337189401d055366a796a5aa0fededeb
root <root@m2.4.local>
2024-10-07 20:53:52 +0000

    Used an alternative method of overriding Knockout.js templates via requirejs-config.js instead of checkout_index_index.xml; However, the preferred way of overriding a template is via checkout_index_index.xml for extensibility.

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -2,4 +2,11 @@ let config = {
     deps: [
         'Denal05_MacademyCustomizeM2Checkout/js/mask-telephone-inputs'
     ]
+    // For extensibility, we always prefer the checkout_index_index.xml way instead of the requirejs-config.js way:
+    //, map: {
+    //     '*': {
+    //         'Magento_Checkout/template/sidebar.html':
+    //             'Denal05_MacademyCustomizeM2Checkout/template/sidebar.html'
+    //     }
+    // }
 };
