6b696db7712d07354cc2b9544d494b1c4b28062d
Denko Aleksandrov <denis.aleksandrov.sa@emeal.nttdata.com>
2024-10-09 19:39:55 +0200

    Extended a custom checkout UI component using a JavaScript mixin, overriding Magento_Checkout/js/view/summary/cart-items 	modified:   view/frontend/layout/checkout_index_index.xml 	new file:   view/frontend/web/js/view/summary/cart-items.js

---
+++ view/frontend/web/js/view/summary/cart-items.js
@@ -0,0 +1,14 @@
+define([
+    'Magento_Checkout/js/view/summary/cart-items'
+    ], function(
+        Component
+    ){
+        'use strict';
+
+        return Component.extend({
+            isItemsBlockExpanded: function () {
+                return true;
+            }
+        });
+    }
+);
