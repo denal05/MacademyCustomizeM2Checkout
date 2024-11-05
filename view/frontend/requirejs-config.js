a811b6e5cf2b1c3ab43df085f96d867894df3561
Denis Aleksandrov <denal05@gmail.com>
2024-11-05 11:34:40 +0100

    Update requirejs-config.js

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -2,7 +2,12 @@ let config = {
     deps: [
         'Denal05_MacademyCustomizeM2Checkout/js/mask-telephone-inputs'
     ]
-    // For extensibility, we always prefer the checkout_index_index.xml way instead of the requirejs-config.js way:
+    /*
+    // For extensibility, we always prefer the checkout_index_index.xml way instead of the requirejs-config.js way.
+    // When overriding JavaScript files with the map, we usually don’t define an extension, as it will assume .js 
+    // by default. However, in this case, we are not overriding a JavaScript file but a Knockout HTML file. So, we will
+    // supply the full file name, such as sidebar.html.
+    */
     //, map: {
     //     '*': {
     //         'Magento_Checkout/template/sidebar.html':
